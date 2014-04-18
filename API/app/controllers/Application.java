25ce8d38a267f91009d622086e46b02be7c0593c
Daniel Parker <dparker.tech@gmail.com>
2014-04-18 20:47:32 +1000

    ADDED: Support for CORS by adding a preflight method that accepts all OPTIONS http requests and sets headers so that cross origin requests can be performed

--- API/app/controllers/Application.java
+++ API/app/controllers/Application.java
@@ -12,7 +12,14 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public static Result checkPreFlight(String all) {
+    	response().setHeader("Access-Control-Allow-Origin", "*");
+	response().setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
+	response().setHeader("Access-Control-Allow-Headers", "accept, origin, Content-type, x-json, x-prototype-version, x-requested-with");
+	return ok();
+}
     public static Result newVisModel() {
+    	response().setHeader("Access-Control-Allow-Origin", "*");
     	VisualModel vModel = new VisualModel();
     	vModel.save();
  	return created(
@@ -21,17 +28,19 @@ public class Application extends Controller {
     }
 
     public static Result updateVisModel(Long id) {
-
+    	response().setHeader("Access-Control-Allow-Origin", "*");
     	return ok(
     		VisualModel.find.where().eq("Id", id).findUnique().id.toString()
     	);
     }
 
     public static Result jar(Long id) {
+    	response().setHeader("Access-Control-Allow-Origin", "*");
     	return ok();
     }
 
     public static Result dsl(Long id) {
+    	response().setHeader("Access-Control-Allow-Origin", "*");
     	return ok();
     }
 }
