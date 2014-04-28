5abfbec33bd589db37e4e5b042fb5b818f0fee35
Daniel Parker <dparker.tech@gmail.com>
2014-04-28 12:14:01 +1000

    FIX: W.I.P Syntax errors and missing dependencies for javaParse.java. Class implementation has not been tested.

--- API/app/models/javaParse.java
+++ API/app/models/javaParse.java
@@ -1,59 +1,66 @@
 package models;
 
+import javax.tools.*;
 import java.io.*;
 import java.util.*;
 import java.util.jar.*;
+import org.stringtemplate.v4.*;
+import org.stringtemplate.v4.misc.*;
 
-public class javaParse{
-    public static void main(String[] args)
-        {
-        try{
+public class javaParse {
+    public static void main(String[] args) {
+        try {
             run();
-            } catch (IOException e) {
-            }
-        }
+        } catch (IOException e) {
 
-    public static void run() throws IOException
-        {
-          Manifest manifest = new Manifest();
-          manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0\n");
-          manifest.getMainAttributes().put("Main-Class: NodeOutput\n");
-          JarOutputStream target = new JarOutputStream(new FileOutputStream("output.jar"), manifest);
-          
-          classCompile
-          
-          add(new File("NodeOutput.class"), target); //Placeholder until java file is generated
-          //write to file "System.lnOut("$node1$ linked to $node2$") or something similar using stringTemplate
-          
-          target.close();
         }
+    }
+
+    public static void run() throws IOException {
+        Manifest manifest = new Manifest();
+        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS,"NodeOutput");
+        JarOutputStream target = new JarOutputStream(new FileOutputStream("output.jar"), manifest);
+
+        classCompile(createJavaFile());
+
+        add(new File("NodeOutput.class"), target); //Placeholder until java file is generated
+        //write to file "System.lnOut("$node1$ linked to $node2$") or something similar using stringTemplate
+
+        target.close();
+    }
         
-        public static file createJavaFile(){
-            StringTemplate source = 
-                new StringTemplate("class javaPrint{\npublic static void main(String[] args){\nSystem.out.println(\"$node1$ is connected to $node2$\");\n }\n}");
-            source.setAttribute("node1","");
-            source.setAttribute("node2","");
-            new File("NodeOutput.java");
-        }
+    public static File createJavaFile() throws IOException {
+        ST source =
+            new ST("class NodeOutput{\npublic static void main(String[] args){\nSystem.out.println(\"$node1$ is connected to $node2$\");\n }\n}");
+        source.add("node1","");
+        source.add("node2","");
+
+        File sourceFile = new File("NodeOutput.java");
+        sourceFile.createNewFile();
+        ErrorBuffer errBuf = new ErrorBuffer();
+        source.write(sourceFile, errBuf);
+
+        return sourceFile;
+    }
         
-        //To compile a java file into a class file for executing in the .jar file
-        //possibly return as a file from cache
-        public static void classCompile(file fileToCompile)
-        {
-            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-            //StandardJavaFileManager = fileManager = compiler.getStandardFileManager(null, null, null);
-            int compilationResult = compiler.run(null, null, null, fileToCompile.getPath());
-            if (compilationResult==0){
-                System.out.println("Success.");
-            }   else {
-                System.out.println("Failed.");
-            }
+    //To compile a java file into a class file for executing in the .jar file
+    //possibly return as a file from cache
+    public static void classCompile(File fileToCompile) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        //StandardJavaFileManager = fileManager = compiler.getStandardFileManager(null, null, null);
+        int compilationResult = compiler.run(null, null, null, fileToCompile.getPath());
+        if (compilationResult==0) {
+            System.out.println("Success.");
+        } else {
+            System.out.println("Failed.");
         }
+    }
         
     //parse json to retrieve attributes
-    public static void jsonParse(JsonNode json){
-        json.get(
-    }
+    /*public static void jsonParse(JsonNode json){
+        *//*json.get();*//*
+    }*/
     
     //Adds a file to a jaroutputstream
     private static void add(File source, JarOutputStream target) throws IOException
@@ -97,6 +104,6 @@ public class javaParse{
       {
         if (in != null)
           in.close();
-      }
     }
+  }
 }
